8b05027ef70ac711b84712e48f13406f5e34bbf2
Robert Jaramillo <robert.jaramillo0484@gmail.com>
2023-01-23 14:50:37 -0500

    Cleaned up some comments in order to make things more consistent.

--- report_builder.py
+++ report_builder.py
@@ -61,7 +61,7 @@ class ReportGenerator:
            text - The text to be displayed  
         '''
         if text is None:
-           print("No text "
+           print("No text")
            return -1
         
         self.pdf.add_page()
@@ -72,7 +72,11 @@ class ReportGenerator:
         return 0
 
     def create_paragraph(self, text=None):
-
+        '''Creates a paragraph in the pdf. 
+    
+           Args: 
+           text - The text to be displayed  
+        '''
         if text is None:
             return 0 
 
@@ -84,16 +88,33 @@ class ReportGenerator:
         return 0
 
     def add_image(self, path=None):
+        '''Adds a image to the pdf. 
+    
+           Args: 
+           text - The text to be displayed  
+        '''
         if path is None:  
             print("Please specify a valid path")
-            return 
+            return -1
         
         self.pdf.ln()
         self.pdf.image(name=path)
+        return 0
 
     def add_vertical_bar_graph(self, title=None, label_vals=None, data_vals=None, 
                                x_label=None, y_label=None, width=2, height=2.2, MyDpi=120):
-        
+        '''Creates a vertical bar graph in the pdf. 
+    
+           Args: 
+           title - The title of the chart
+           label_vals - The values for the various labels of the graph
+           data_vals - The data being charted 
+           x_label - The charts x-label
+           y_label - The charts y-label
+           width - Width of the charts in inches 
+           Heigth - Heigth  of the charts in inches 
+           MyDpi - The DPI for the rendering of the chart  
+        '''
         if label_vals is None:
             print("Labes must be specified and cannot be None")
             return -1
@@ -124,7 +145,18 @@ class ReportGenerator:
 
     def add_horizontal_bar_graph(self, title=None, label_vals=None, data_vals=None, 
                                  x_label=None, y_label=None, width=3, height=2, MyDpi=120):
-        
+        '''Creates a horizontal bar graph in the pdf. 
+    
+           Args: 
+           title - The title of the chart
+           label_vals - The values for the various labels of the graph
+           data_vals - The data being charted 
+           x_label - The charts x-label
+           y_label - The charts y-label
+           width - Width of the charts in inches 
+           Heigth - Heigth  of the charts in inches 
+           MyDpi - The DPI for the rendering of the chart  
+        '''
         if label_vals is None:
             print("Labes must be specified and cannot be None")
             return -1
@@ -153,6 +185,16 @@ class ReportGenerator:
         return 0
 
     def add_pie_chart(self, title=None, chart_labels=None, values=None, width=3, height=2, MyDpi=120):
+        '''Creates a pie chart in the pdf. 
+    
+           Args: 
+           title - The title of the chart
+           chart_labels - The values for the various labels of the graph
+           values - The data being charted 
+           width - Width of the charts in inches 
+           Heigth - Heigth  of the charts in inches 
+           MyDpi - The DPI for the rendering of the chart  
+        '''
 
         if chart_labels is None:  
            print("Labels, cannot be none and must be specified.")
@@ -176,8 +218,20 @@ class ReportGenerator:
 
         return 0
     
-    def add_line_chart(self, title=None, xlabel='', ylabel='', label_vals=None, data_vals=None,
+    def add_line_chart(self, title=None, x_label='', y_label='', label_vals=None, data_vals=None,
                        width=3, height=2, MyDpi=120):
+        '''Creates a line graph in the pdf. 
+    
+           Args: 
+           title - The title of the chart
+           label_vals - The values for the various labels of the graph
+           data_vals - The data being charted 
+           x_label - The charts x-label
+           y_label - The charts y-label
+           width - Width of the charts in inches 
+           Heigth - Heigth  of the charts in inches 
+           MyDpi - The DPI for the rendering of the chart  
+        '''
 
         if title is None:
            print("A title needs to be provided")
@@ -193,8 +247,8 @@ class ReportGenerator:
            return -1
 
         plt.figure(figsize=(width, height), dpi=MyDpi)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
         plt.title(title)
         plt.plot(label_vals, data_vals)
         file_name = title+'.png'
